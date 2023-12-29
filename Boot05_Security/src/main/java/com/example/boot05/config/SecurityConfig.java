50d236573e939b5f976f6d06dc3411eebdd04342
STJ <ssongtj0505@gmail.com>
2023-12-29 12:00:30 +0900

    231229 수업

--- Boot05_Security/src/main/java/com/example/boot05/config/SecurityConfig.java
+++ Boot05_Security/src/main/java/com/example/boot05/config/SecurityConfig.java
@@ -18,10 +18,12 @@ public class SecurityConfig {
 	@Bean //메소드에서 리턴되는 SecurityFilterChain 을 bean으로 만들어준다.
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 		//화이트 리스트를 미리 배열에 넣어두기
-		String[] whiteList = {"/","/play","/notice","/user/loginform"};
+		String[] whiteList = {"/","/play","/notice","/user/loginform","/user/login_fail"};
 		
 		//메소드의 매개변수에 HttpSecurity 의 참조값이 전달되는데 해당 객체를 이용해서 설정을 한다음
-		httpSecurity.authorizeHttpRequests(config -> 
+		httpSecurity
+		.csrf(csrf -> csrf.disable())
+		.authorizeHttpRequests(config -> 
 			config
 				.requestMatchers(whiteList).permitAll() //whiteList 요청은 로그인과 상관없이 모두 허용
 				.requestMatchers("/admin/**").hasRole("ADMIN") // admin 이 아니면 admin의 하위 페이지 요청 불가
@@ -30,10 +32,19 @@ public class SecurityConfig {
 		)
 		.formLogin(config ->
 		config
+			//로그인 처리를 할 때 요청될 URL 설정 ( spring security 가 login 처리를 대신 해준다. )
 			.loginProcessingUrl("/user/login")
+			//로그인 처리를 대신 하려면 어떤 파라미터명으로 username 과 password 가 넘어오는지 알려주기
 			.usernameParameter("userName")
 			.passwordParameter("password")
-			.successForwardUrl("/user/login_success")
+			.successForwardUrl("/user/login_success")// 로그인 성공 후 forward 될 url 설정
+			.failureForwardUrl("/user/login_fail")
+			.permitAll() // 위에 명시한 모든 요청경로를 로그인 없이 요청할 수 있도록 설정
+		)
+		.logout(config ->
+		config
+			.logoutUrl("/user/logout")//spring security 가 logout 처리를 대신 해준다.
+			.logoutSuccessUrl("/") //로그아웃 이후에 리다이렉트 시킬 경로 설정
 			.permitAll()
 		);
 		//설정된 정보대로 SecurityFilterChain 객체를 만들어서 반환한다.
--- Boot05_Security/src/main/java/com/example/boot05/controller/UserController.java
+++ Boot05_Security/src/main/java/com/example/boot05/controller/UserController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	
+	//로그인폼을 제출(post) 한 로그인 프로세스 중에 forward 되는 경로이기 때문에 @PostMapping임 주의!
+	@PostMapping("/user/login_fail")
+	public String loginFail() {
+		//로그인 실패임을 알릴 페이지
+		return "user/login_fail";
+	}
 	@GetMapping("/user/loginform")
 	public String loginform() {
 		//templates/user/loginform.html 페이지로 forward 이동해서 응답
